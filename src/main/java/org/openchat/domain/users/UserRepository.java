c0621a3947caae95309afc2ad680f1795c0ea436
Paulo Clavijo Esteban <paucls@gmail.com>
2018-12-31 15:52:33 +0000

    Implemented UserRepository should inform when username is already taken (2 min15)

--- src/main/java/org/openchat/domain/users/UserRepository.java
+++ src/main/java/org/openchat/domain/users/UserRepository.java
@@ -1,11 +1,16 @@
 package org.openchat.domain.users;
 
+import java.util.HashMap;
+
 public class UserRepository {
+
+    private final HashMap<String, User> users = new HashMap();
+
     public void add(User user) {
-        throw new UnsupportedOperationException();
+        users.put(user.username(), user);
     }
 
     public boolean isUsernameTaken(String username) {
-        throw new UnsupportedOperationException();
+        return users.containsKey(username);
     }
 }
--- src/test-integration/java/org/openchat/domain/users/UserRepositoryShould.java
+++ src/test-integration/java/org/openchat/domain/users/UserRepositoryShould.java
@@ -1,17 +1,26 @@
 package org.openchat.domain.users;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.openchat.infrastructure.builders.UserBuilder.aUser;
 
 public class UserRepositoryShould {
+    private static final User JOHN = aUser().withUsername("john").build();
+    private static final User CHARLIE = aUser().withUsername("charlie").build();
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() {
+        userRepository = new UserRepository();
+    }
+
     @Test
     public void inform_when_username_is_already_taken() {
-        UserRepository userRepository = new UserRepository();
-        String username = "john";
+        userRepository.add(JOHN);
 
-        boolean isUsernameTaken = userRepository.isUsernameTaken(username);
-
-        assertThat(isUsernameTaken).isTrue();
+        assertThat(userRepository.isUsernameTaken(JOHN.username())).isTrue();
+        assertThat(userRepository.isUsernameTaken(CHARLIE.username())).isFalse();
     }
 }
